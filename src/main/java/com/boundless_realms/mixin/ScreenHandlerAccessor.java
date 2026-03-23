ea6b14e6822def2455e9f2d95bad3c6fb61807e5
Holly Yi <hollyyiishere@gmail.com>
2026-03-23 18:00:41 +0800

    Changed some mechanics of Bedrock Hand Item

--- src/main/java/com/boundless_realms/mixin/ScreenHandlerAccessor.java
+++ src/main/java/com/boundless_realms/mixin/ScreenHandlerAccessor.java
@@ -2,11 +2,16 @@ package com.boundless_realms.mixin;
 
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
+import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ScreenHandler.class)
 public interface ScreenHandlerAccessor {
+    @Accessor("slots")
+    DefaultedList<Slot> boundlessRealms$getSlots();
+
     @Invoker("addSlot")
     Slot boundlessRealms$invokeAddSlot(Slot slot);
 }
