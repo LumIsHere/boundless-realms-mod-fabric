a9e22985cba504ef73874396a2ba739a75a09378
LumIsHere <lyuanze2011@gmail.com>
2026-03-24 07:55:22 +0800

    New Bitcoin Miner Block

---
+++ src/main/java/com/boundless_realms/block/BitcoinMinerBlock.java
@@ -0,0 +1,25 @@
+package com.boundless_realms.block;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
+import org.jspecify.annotations.Nullable;
+
+public class BitcoinMinerBlock extends BlockWithEntity implements BlockEntityProvider {
+    public BitcoinMinerBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    protected MapCodec<? extends BlockWithEntity> getCodec() {
+        return null;
+    }
+
+    @Override
+    public @Nullable BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return null;
+    }
+}
